10cd3b2556f28afb2ac4060e148254a17ee86397
Carlos Fabiano <carlosfabianofrancisco@gmail.com>
2024-07-22 13:47:26 -0300

    Ajustando a conta

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # Login no AWS ECR
-aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin <your-aws-account-id>.dkr.ecr.us-east-1.amazonaws.com
+aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 135218620329.dkr.ecr.us-east-1.amazonaws.com
